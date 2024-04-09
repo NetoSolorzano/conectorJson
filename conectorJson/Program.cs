46c87e67d83bcf5747d84dba66c5cc72c4c0625e
Lucio Solórzano <lucio.solorzano@gmail.com>
2024-04-09 10:30:45 -0500

    cambio apisperu 09/04/2024

--- conectorJson/Program.cs
+++ conectorJson/Program.cs
@@ -21,18 +21,22 @@ namespace conectorJson
             }
             else
             {
+                #region ruc.com.pe
+                /* metodo con ruc.com.pe
                 string numero = args[0].ToString();
                 // create a request
                 ServicePointManager.SecurityProtocol = System.Net.SecurityProtocolType.Tls12;
                 var httpWebRequest = (HttpWebRequest)WebRequest.Create("https://ruc.com.pe/api/v1/consultas");
                 httpWebRequest.ContentType = "application/json";
                 httpWebRequest.Method = "POST";
+
                 if (args[1].ToString() == "0")
                 {
                     using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
                     {
                         string json = "{\"token\":\"f193780e-9239-464e-918d-548f96210a39-364baff6-543f-4cc7-a783-047d9c820d7e\"," +
                                       "\"ruc\":\"" + numero + "\"}";
+                        string json = numero + "{\"token\":\"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6Imx1Y2lvLnNvbG9yemFub0BnbWFpbC5jb20ifQ.ZVIcx1EuIzl9xiSbUTYVRru4vJI-KpjXYauXhGsL_wk" + "\"}";
                         streamWriter.Write(json);
                     }
                     var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
@@ -40,31 +44,6 @@ namespace conectorJson
                     {
                         var result = streamReader.ReadToEnd();
                         var masticado = JObject.Parse(result);
-                        /*
-                        Console.WriteLine(masticado["success"]);
-                        Console.WriteLine(masticado["ruc"]);
-                        Console.WriteLine(masticado["nombre_o_razon_social"]);
-                        Console.WriteLine(masticado["estado_del_contribuyente"]);
-                        Console.WriteLine(masticado["condicion_de_domicilio"]);
-                        Console.WriteLine(masticado["ubigeo"]);
-                        Console.WriteLine(masticado["tipo_de_via"]);
-                        Console.WriteLine(masticado["nombre_de_via"]);
-                        Console.WriteLine(masticado["codigo_de_zona"]);
-                        Console.WriteLine(masticado["tipo_de_zona"]);
-                        Console.WriteLine(masticado["numero"]);
-                        Console.WriteLine(masticado["interior"]);
-                        Console.WriteLine(masticado["lote"]);
-                        Console.WriteLine(masticado["dpto"]);
-                        Console.WriteLine(masticado["manzana"]);
-                        Console.WriteLine(masticado["kilometro"]);
-                        Console.WriteLine(masticado["departamento"]);
-                        Console.WriteLine(masticado["provincia"]);
-                        Console.WriteLine(masticado["distrito"]);
-                        Console.WriteLine(masticado["direccion"]);
-                        Console.WriteLine(masticado["direccion_completa"]);
-                        Console.WriteLine(masticado["ultima_actualizacion"]);
-                        */
-                        //Console.ReadKey();
                         string datos = "";
                         if (masticado["success"].ToString() == "True")
                         {
@@ -83,15 +62,15 @@ namespace conectorJson
                 }
                 else
                 {   // args[1].ToString() == "1"
-                    using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
-                    {
-                        string json = "{\"token\":\"f193780e-9239-464e-918d-548f96210a39-364baff6-543f-4cc7-a783-047d9c820d7e\"," +
-                                      "\"dni\":\"" + numero + "\"}";
-                        streamWriter.Write(json);
-                    }
                     string datos = "";
                     try
                     {
+                        using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+                        {
+                            string json = "{\"token\":\"f193780e-9239-464e-918d-548f96210a39-364baff6-543f-4cc7-a783-047d9c820d7e\"," +
+                                          "\"dni\":\"" + numero + "\"}";
+                            streamWriter.Write(json);
+                        }
                         var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
                         using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
                         {
@@ -115,6 +94,76 @@ namespace conectorJson
                     Console.WriteLine(datos);
                 }
                 //Console.ReadKey();
+                */
+                #endregion
+                #region apisperu
+                string numero = args[0].ToString();
+                // create a request
+                ServicePointManager.SecurityProtocol = System.Net.SecurityProtocolType.Tls12;
+                string token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6Imx1Y2lvLnNvbG9yemFub0BnbWFpbC5jb20ifQ.ZVIcx1EuIzl9xiSbUTYVRru4vJI-KpjXYauXhGsL_wk";
+                if (args[1].ToString() == "0")
+                {
+                    var httpWebRequest = (HttpWebRequest)WebRequest.Create("https://dniruc.apisperu.com/api/v1/ruc/" + numero + "?token=" + token);
+                    httpWebRequest.ContentType = "application/json";
+                    httpWebRequest.Method = "GET";
+                    var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+                    using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                    {
+                        var result = streamReader.ReadToEnd();
+                        var masticado = JObject.Parse(result);
+                        string datos = "";
+                        if (masticado.HasValues == true)
+                        {
+                            datos = masticado["ruc"].ToString() + "|" +
+                                masticado["razonSocial"].ToString() + "|" +
+                                masticado["estado"].ToString() + "|" +
+                                masticado["condicion"].ToString() + "|" +
+                                masticado["ubigeo"].ToString() + "|" +
+                                masticado["direccion"].ToString() + "|" +
+                                masticado["distrito"].ToString() + "|" +
+                                masticado["provincia"].ToString() + "|" +
+                                masticado["departamento"].ToString();
+                        }
+                        Console.WriteLine(datos);
+                    }
+                }
+                else
+                {   // args[1].ToString() == "1"
+                    string datos = "";
+                    try
+                    {
+                        var httpWebRequest = (HttpWebRequest)WebRequest.Create("https://dniruc.apisperu.com/api/v1/dni/" + numero + "?token=" + token);
+                        httpWebRequest.ContentType = "application/json";
+                        httpWebRequest.Method = "GET";
+                        var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+                        using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                        {
+                            var result = streamReader.ReadToEnd();
+                            var masticado = JObject.Parse(result);
+                            if (masticado.HasValues == true)
+                            {
+                                if (masticado["success"].ToString().ToLower() == "true")
+                                {
+                                    datos = masticado["dni"].ToString().Trim() + "|" +
+                                        masticado["nombres"].ToString().Trim() + " " +
+                                        masticado["apellidoPaterno"].ToString().Trim() + " " +
+                                        masticado["apellidoMaterno"].ToString().Trim();
+                                }
+                            }
+                            else
+                            {
+                                datos = "00000000".ToString().Trim() + "|" + "CLIENTE NO IDENTIFICADO";
+                            }
+                        }
+                    }
+                    catch
+                    {
+                        datos = "00000000".ToString().Trim() + "|" + "CLIENTE NO IDENTIFICADO";
+                    }
+                    Console.WriteLine(datos);
+                    //Console.ReadKey();
+                }
+                #endregion
             }
         }
     }
